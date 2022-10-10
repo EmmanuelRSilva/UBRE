a5b5c44e15d47e2af459568f7e62f7c59b108a8a
Emmanuel Rodrigues da Silva <emmanuel.silva@mereo.com>
2022-10-10 09:41:52 -0300

    Teste commit
    Testando o corpo do commit aqui

--- index.php
+++ index.php
@@ -100,23 +100,7 @@
 				carregaLogin("libs/login.php","#form-visitante",".resultado-login");
 				return false;
 			});	
-
-			// window.fbAsyncInit = function() {
-			//     FB.init({
-			//       appId      : '845906538832145',
-			//       xfbml      : true,
-			//       version    : 'v2.3'
-			//     });
-			//   };
-
-			//   (function(d, s, id){
-			//      var js, fjs = d.getElementsByTagName(s)[0];
-			//      if (d.getElementById(id)) {return;}
-			//      js = d.createElement(s); js.id = id;
-			//      js.src = "//connect.facebook.net/en_US/sdk.js";
-			//      fjs.parentNode.insertBefore(js, fjs);
-			//    }(document, 'script', 'facebook-jssdk'));	
-
+		
 		</script>
 	</body>
 </html>
